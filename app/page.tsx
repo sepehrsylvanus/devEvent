e0398f79cf22c23e36dfd8953772e20bd226722f
sepehrsylvanus <mr.forte.world@gmail.com>
2026-06-25 20:11:27 +0330

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        هر رویدادی که
+        <br />
+        نباید از دست بدی
+      </h1>
+      <p className="mt-5 text-center">میتاپ ها و کنفرانس ها - همگی در یک جا</p>
+    </section>
+  );
+};
+
+export default page;
