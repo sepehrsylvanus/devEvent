c2caf364d2e8b291a8b57370a7ca2798307258c4
sepehrsylvanus <mr.forte.world@gmail.com>
2026-06-26 14:55:46 +0330

    add explore button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import ExploreBtn from "@/components/ExploreBtn";
 import React from "react";
 
 const page = () => {
-  return <div>Welcome to next.js</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-7"></div>
+      <h3>Featured Events</h3>
+      <ul className="events">Events</ul>
+    </section>
+  );
 };
 
 export default page;
