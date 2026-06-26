8876a4901a2da95970db2d3c06bf2cb8448a6b9e
sepehrsylvanus <mr.forte.world@gmail.com>
2026-06-26 13:24:36 +0330

    project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return (
-    <section>
-      <h1 className="text-center">
-        هر رویدادی که
-        <br />
-        نباید از دست بدی
-      </h1>
-      <p className="mt-5 text-center">میتاپ ها و کنفرانس ها - همگی در یک جا</p>
-    </section>
-  );
+  return <div>Welcome to next.js</div>;
 };
 
 export default page;
